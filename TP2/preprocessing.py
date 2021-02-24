37f15ed620d9408d70b46cdef5e660e9f321dcaf
JuanOpizzi <juanopizzi@gmail.com>
2021-02-24 16:46:56 -0300

    hago gran refactor de prepocesing.oy
    paso todas las funciones a un archivo .py como coresponde, y no un notebook como estaba antes. Corrijo estos mismos cambios en el modelo de knn, hay que hacerlo en todos, hago un segundo procesado en knn y subo ambas predicciones del modelo

--- TP2/preprocessing.py
+++ TP2/preprocessing.py
@@ -1,36 +1,5 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.9.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# # Preprocessing TP1
-# Aca se escriben los distintos preprocesados y se describe rapidamente su funcionalidad
-#
-
-# +
-import pandas as pd
-from matplotlib import pyplot as plt
-import matplotlib
-import seaborn as sns
 import numpy as np
-np.warnings.filterwarnings('ignore')
-from sklearn.metrics import accuracy_score
-import category_encoders as ce
-
-sns.set()
-
-
-# -
+import pandas as pd
 
 column_titles = ["tipo_de_sala","id_usuario","genero","edad","amigos",
                  "parientes","precio_ticket",'nombre_sede','cant_acompañantes','volveria']
@@ -38,49 +7,38 @@ column_titles = ["tipo_de_sala","id_usuario","genero","edad","amigos",
 numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 
 
-# Este preprossing esta basado en lo visto y hecho en el TP1, y es nuestro primero approach de preprossing
+def eliminar_features_que_no_aportan_info(df, drop_fila = False):
+    if drop_fila == True:
+        df = df.drop(['id_ticket', 'fila', 'nombre'], axis=1)
+    else:
+        df = df.drop(['id_ticket', 'nombre'], axis=1)
+    return df
 
-def generate_initial_dataset(data_path, decision_path):
-    df_datos = pd.read_csv(data_path)
-    df_predict = pd.read_csv(decision_path)
-    df = pd.merge(df_datos, df_predict, how='inner', left_on='id_usuario', right_on='id_usuario')
-    df = df.drop(['id_ticket', 'fila', 'nombre'], axis=1)
+def crear_feature_acompañantes(df):
     df['cant_acompañantes'] = df['parientes'] + df['amigos']
-    df = df.reindex(columns = column_titles)
     return df
 
+# reemplaza los valores nulos de la columna que se pasa
+# si la columna no existe en el data set no hace nada
+def replace_nulls_column(df, columna, metrica):
+    if columna not in df.columns:
+        return
+    else:
+        s = df[columna]
+        if metrica == 'moda':
+            df = df.replace({columna: np.nan}, s.mode())
+        elif metrica == 'mediana':
+            df = df.replace({columna: np.nan}, s.median())
+        elif metrica == 'media':
+            df = df.replace({columna: np.nan}, s.mean())
+        df = df.round({columna: 1})
+        return df
 
-def generate_holdout_dataset(df_path):
-    df = pd.read_csv(df_path)
-    df = df.drop(['id_ticket', 'fila', 'nombre'], axis=1)
-    df['cant_acompañantes'] = df['parientes'] + df['amigos']
-    df = df.reindex(columns = column_titles)
-    return df
-
-
-# Reemplazo los valores nulos de edad con la moda, la media o la meediana, segun que quiera
-
-def replace_nulls_edad(df, metrica):
-    s = df['edad']
-    if metrica == 'moda':
-        df = df.replace({'edad': np.nan}, s.mode())
-    elif metrica == 'mediana':
-        df = df.replace({'edad': np.nan}, s.median())
-    elif metrica == 'media':
-        df = df.replace({'edad': np.nan}, s.mean())
-    df = df.round({'edad': 1})
-    return df
-
-
-# Encodeo todos los atributos categoricos
-
+# con dummy_na pasamos nan a categoria y con drop_first eliminamos una columna, que a fines
+# practicos es informacion redundante
 def encodear_atributos_categoricos(df):
-    encoder = ce.BinaryEncoder(cols=['tipo_de_sala', 'genero', 'nombre_sede'],return_df=True)
-    df = encoder.fit_transform(df) 
-    return df
-
-
-# Agarro todos los atributos numericos y los normalizo
+    encodeado = pd.get_dummies(df, columns=['tipo_de_sala', 'genero', 'nombre_sede'], dummy_na=True, drop_first=True)
+    return encodeado
 
 def normalizar_atributos_numericos(df):
     columnas_numericas = df.select_dtypes(include=numerics).columns.to_list()
@@ -88,9 +46,3 @@ def normalizar_atributos_numericos(df):
     minimo = df[columnas_numericas].min()
     df[columnas_numericas] = (df[columnas_numericas] - df[columnas_numericas].min()) / (df[columnas_numericas].max() - df[columnas_numericas].min())
     return df
-
-
-
-
-
-
